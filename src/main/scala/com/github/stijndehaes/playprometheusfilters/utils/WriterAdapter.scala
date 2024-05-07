f16a4dab1af0ef5d613e4f39c214c3baff501acc
Jan Kurella <kurellajunior@gmail.com>
2024-05-07 16:25:35 +0200

    avoid unnecessary en- and decoding
    As a thank you. This is how I rewrote it for me

--- src/main/scala/com/github/stijndehaes/playprometheusfilters/utils/WriterAdapter.scala
+++ src/main/scala/com/github/stijndehaes/playprometheusfilters/utils/WriterAdapter.scala
@@ -5,7 +5,7 @@ import java.io.Writer
 class WriterAdapter(buffer: StringBuilder) extends Writer {
 
   override def write(charArray: Array[Char], offset: Int, length: Int): Unit =
-    buffer ++= new String(new String(charArray, offset, length).getBytes("UTF-8"), "UTF-8")
+    buffer.appendAll(charArray, offset, length)
 
   override def flush(): Unit = {}
 
